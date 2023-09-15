9d416c5557c9812d7b35abf54bc859abc482c3a9
luis miguel <luismrocha132@gmail.com>
2023-09-14 21:16:50 -0500

    primeros cambios

---
+++ src/main/java/com/miDoctorya/coremiDoctorya/CoreMiDoctoryaApplication.java
@@ -0,0 +1,13 @@
+package com.miDoctorya.coremiDoctorya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreMiDoctoryaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreMiDoctoryaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miDoctorya/coremiDoctorya/CoreMiDoctoryaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miDoctorya.coremiDoctorya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreMiDoctoryaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
